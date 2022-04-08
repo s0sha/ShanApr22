e9d4c53440c338e0aa0413deb0fedc991e112307
Shankari <shankari99@yahoo.com>
2022-04-08 13:43:30 +1000

    First Commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+using System;
+
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://horse.industryconnect.io/Account/login?ReturnUrl=%2f");
+            driver.Manage().Window.Maximize();
+
+            IWebElement usernameText = driver.FindElement(By.Id("UserName"));
+
+            usernameText.SendKeys("hari");
+
+            IWebElement passwordText = driver.FindElement(By.Id("Password"));
+            passwordText.SendKeys("123123");
+
+            IWebElement loginButton = driver.FindElement(By.XPath("//*[@id='loginForm']/form/div[3]/input[1]"));
+            loginButton.Click();
+
+            IWebElement helloHari = driver.FindElement(By.XPath("//*[@id='logoutForm']/ul/li/a"));
+            if (helloHari.Text == "Hello hari!")
+            {
+
+                Console.WriteLine("Test Passed");
+            }
+            else { Console.WriteLine("Test Failed"); }
+                
+                    
+                    
+        }
+    }
+}
